2d9cbe492661134f4de606e57729111c80620aef
kurtis-s <kurtis_s@hotmail.com>
2014-04-13 21:41:38 -0700

    used vectorized regularized logistic regression from last week

--- mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3/lrCostFunction.m
@@ -35,7 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-J = (1/m)*sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta)));
+J = (1/m)*sum((-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta)))) ...
+        + (lambda/(2*m))*sum(theta(2:end, :).^2);
+z_theta = theta; z_theta(1,1)=0;
+grad = (1/m)*(((sigmoid(X*theta)-y)'*X)' + lambda*z_theta);
 % =============================================================
 
 grad = grad(:);
