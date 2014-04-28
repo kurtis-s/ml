67f85fc378ac434604dfb327355632f9119bbc3e
kurtis-s <kurtis_s@hotmail.com>
2014-04-27 22:42:27 -0700

    added optional exercise to calculate error for test set

--- mlclass-ex5/ex5.m
+++ mlclass-ex5/ex5.m
@@ -164,7 +164,7 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 3;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
@@ -218,3 +218,7 @@ end
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+%% =========== Part 9: Optional calculate test set error =============
+J = linearRegCostFunction(X_poly_test, ytest, theta, 0);
+fprintf('Test set error\n');
+display(J);
