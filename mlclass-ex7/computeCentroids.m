5f4cc5bbc6484f4050cdfcaea87f78991cadc5dc
kurtis-s <kurtis_s@hotmail.com>
2014-05-11 16:23:31 -0700

    added code to compute the new centroids after assigning x vals

--- mlclass-ex7/computeCentroids.m
+++ mlclass-ex7/computeCentroids.m
@@ -1,5 +1,5 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
 %data points assigned to each centroid.
 %   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
 %   computing the means of the data points assigned to each centroid. It is
@@ -25,16 +25,11 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
-
+for k=1:K,
+    assigned_x_vals = X(idx==k, :);
+    centroids(k, :) = mean(assigned_x_vals);
+end
 % =============================================================
 
 
 end
-
